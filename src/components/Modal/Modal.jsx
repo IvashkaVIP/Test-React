c1acf4ec2284b293f7e031521f8c4c1e3ecd87b0
IvashkaVIP <ivashka.co@gmail.com>
2023-07-08 10:05:15 +0300

    Modal add clicker backdrop

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,7 +3,6 @@ import { Component } from 'react';
 import './Modal.scss';
 
 export class Modal extends Component {
-  
   componentDidMount() {
     //console.log(this.props);
     console.log('Modal >>> DidMount');
@@ -12,20 +11,27 @@ export class Modal extends Component {
 
   componentWillUnmount() {
     console.log('Modal >>> WillUnmount');
-  window.removeEventListener('keydown',this.handleKeyDown)
-}
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
 
   handleKeyDown = ({ code }) => {
-    if (code === 'Escape'){
-      console.log('Modal >>> handleKeyDown : Escape')
+    if (code === 'Escape') {
+      console.log('Modal >>> handleKeyDown : Escape');
       this.props.onClose();
-  }
-  }
+    }
+  };
+
+  handleBackdropClick = evt => {
+    if (evt.currentTarget === evt.target) {
+      console.log('Modal >>>  Click on BackDrop');
+      this.props.onClose();
+    }
+  };
 
   render() {
     return (
       <>
-        <div className="Modal__backdrop">
+        <div className="Modal__backdrop" onClick={this.handleBackdropClick}>
           <div className="Modal__content">{this.props.children}</div>
         </div>
       </>
